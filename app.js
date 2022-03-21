f1c93028193fd038bcfa5819cbef5ee333e4213c
Beau Elliott <belliott1516@gmail.com>
2022-03-21 14:07:32 -0700

    created eventlistener for app.js on load

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+window.addEventListener('load', async() => {
 // on load
+    const allDogs = await getDogs();
 // fetch all dogs
+    for (let dog of allDogs){
+        const dogsEl = renderDogCard(dog);
 // render and append all dog cards to the container
+        dogListContainer.append(dogsEl);
+    }
+});
+
+
